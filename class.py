cdffc00780d8b48cc60ff22dbe11672229cd0422
Mina <146908931+mina-ghanadi@users.noreply.github.com>
2024-02-21 10:12:09 +0330

    add inherit(child) class

--- class.py
+++ class.py
@@ -12,3 +12,15 @@ client1=Client("Mina","mina_gh@yahoo.com",22252193)
 print(client1)
 client2=Client("Mitra","m@gh@yahoo.com","22234564")
 print(client2)
+
+#Add child class
+class VipClient(Client):
+    def __init__(self,name,email,phone,vipNumber):
+        Client.__init__(self,name,email,phone)
+        self.vipNumber=vipNumber
+    def vipcode(self):
+        return (self.vipNumber)
+    
+vip1=VipClient("Jack","mf@yahoo.com",22245329,1562)
+print(vip1)
+print(vip1.vipcode())
